d2b1b938763a30f24de3b620d93699c042234b2d
Maleina Bidek <maleina@ucla.edu>
2021-03-13 09:13:16 -0500

    feat: Wire PlayGame view buttons to outlets & action

--- Roshambo/PlayGame.swift
+++ Roshambo/PlayGame.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class PlayGame: UIViewController {
 
+    @IBOutlet weak var paperButton: UIButton!
+    @IBOutlet weak var rockButton: UIButton!
+    @IBOutlet weak var scissorsButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func playGame(){
+        
+    }
 
 }
 
