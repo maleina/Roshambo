4160259e301ccf20e43c95dcf42cf98b6305db0d
Maleina Bidek <maleina@ucla.edu>
2021-03-13 10:52:33 -0500

    feat: Add game play logic

--- Roshambo/MatchResults.swift
+++ Roshambo/MatchResults.swift
@@ -15,15 +15,33 @@ class MatchResults: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func matchResult(player: String, opponent: String){
+        let image: UIImage!
+        let message: String
+        
+        switch (player, opponent) {
+        case ("paper", "rock"):
+            image = UIImage(named: "PaperCoversRock")
+            message = "Paper covers Rock. You win!"
+        case ("rock", "paper"):
+            image = UIImage(named: "PaperCoversRock")
+            message = "Paper covers Rock. You lose!"
+        case("rock", "scissors"):
+            image = UIImage(named: "RockCrushesScissors")
+            message = "Rock crushes Scissors. You win!"
+        case( "scissors", "rock"):
+            image = UIImage(named: "RockCrushesScissors")
+            message = "Rock crushes Scissors. You lose!"
+        case( "scissors", "paper"):
+            image = UIImage(named: "ScissorsCutPaper")
+            message = "Scissors cut Paper. You win!"
+        case( "paper", "scissors"):
+            image = UIImage(named: "ScissorsCutPaper")
+            message = "Scissors cut Paper. You lose!"
+        default:
+            image = UIImage(named: "itsATie")
+            message = "It's a Tie!"
+        }
     }
-    */
 
 }
--- Roshambo/PlayGame.swift
+++ Roshambo/PlayGame.swift
@@ -12,15 +12,43 @@ class PlayGame: UIViewController {
     @IBOutlet weak var paperButton: UIButton!
     @IBOutlet weak var rockButton: UIButton!
     @IBOutlet weak var scissorsButton: UIButton!
-    
+
+    enum ButtonType: Int {
+        case paper = 0, rock, scissors
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func playGame(){
+    @IBAction func playGame(_ sender: UIButton){
         
+        let player : String
+        let opponent: String = getOpponentPlay()
+        
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .paper:
+            player = "paper"
+        case .rock:
+            player = "rock"
+        case .scissors:
+            player = "scissors"
+        }
+        
+
+
+    }
+    
+    func getOpponentPlay() -> String{
+        let randomValue = 1 + arc4random() % 3
+        switch (randomValue){
+        case 1:
+            return "paper"
+        case 2:
+            return "rock"
+        default: return "scissors"
+        }
     }
 
 }
